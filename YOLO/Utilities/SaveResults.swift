dab00d43111b45443b0e72e473480324330e419d
UltralyticsAssistant <web@ultralytics.com>
2024-08-11 20:23:17 +0000

    Auto-format by https://ultralytics.com/actions

--- YOLO/Utilities/SaveResults.swift
+++ YOLO/Utilities/SaveResults.swift
@@ -8,40 +8,43 @@
 
 import Foundation
 
-let detectionHeader = "sec_day, free_space, batteryLevel ,class,confidence,box_x, box_y, box_w, box_h\n"
-let humanHeader = "sec_day, free_space, battery_level ,id, confidence, box_x, box_y, box_w, box_h, weight, height, age, gender, gender_confidence, race, race_confidence \n"
+let detectionHeader =
+  "sec_day, free_space, batteryLevel ,class,confidence,box_x, box_y, box_w, box_h\n"
+let humanHeader =
+  "sec_day, free_space, battery_level ,id, confidence, box_x, box_y, box_w, box_h, weight, height, age, gender, gender_confidence, race, race_confidence \n"
 
-func saveDetectionResultsToCSV(detectionResults:[String], task: Task) -> URL? {
-    var header = ""
-    var taskName = ""
-    switch task {
-    case .detect:
-        header = detectionHeader
-        taskName = "detection"
+func saveDetectionResultsToCSV(detectionResults: [String], task: Task) -> URL? {
+  var header = ""
+  var taskName = ""
+  switch task {
+  case .detect:
+    header = detectionHeader
+    taskName = "detection"
 
-    case .human:
-        header = humanHeader
-        taskName = "human"
-    }
-    let formatter = DateFormatter()
-    formatter.dateFormat = "yyyyMMdd_HH:mm:ss"
-    let dateString = formatter.string(from: Date())
-    let fileName =  taskName + "_results_\(dateString).csv"
+  case .human:
+    header = humanHeader
+    taskName = "human"
+  }
+  let formatter = DateFormatter()
+  formatter.dateFormat = "yyyyMMdd_HH:mm:ss"
+  let dateString = formatter.string(from: Date())
+  let fileName = taskName + "_results_\(dateString).csv"
 
-    let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
-    
-    var csvText = header
-    
-    for result in detectionResults {
-        csvText.append(contentsOf: result)
-    }
-    
-    do {
-        try csvText.write(to: path, atomically: true, encoding: .utf8)
-        print("CSV file saved at: \(path)")
-        return path
-    } catch {
-        print("Failed to save CSV file: \(error)")
-        return nil
-    }
+  let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    .appendingPathComponent(fileName)
+
+  var csvText = header
+
+  for result in detectionResults {
+    csvText.append(contentsOf: result)
+  }
+
+  do {
+    try csvText.write(to: path, atomically: true, encoding: .utf8)
+    print("CSV file saved at: \(path)")
+    return path
+  } catch {
+    print("Failed to save CSV file: \(error)")
+    return nil
+  }
 }
--- YOLO/VideoCapture.swift
+++ YOLO/VideoCapture.swift
@@ -11,154 +11,165 @@
 //  the capture session. It also provides methods to start and stop video capture and delivers captured frames
 //  to a delegate implementing the VideoCaptureDelegate protocol.
 
-
 import AVFoundation
 import CoreVideo
 import UIKit
 
 // Defines the protocol for handling video frame capture events.
 public protocol VideoCaptureDelegate: AnyObject {
-    func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame: CMSampleBuffer)
+  func videoCapture(_ capture: VideoCapture, didCaptureVideoFrame: CMSampleBuffer)
 }
 
 // Identifies the best available camera device based on user preferences and device capabilities.
 func bestCaptureDevice() -> AVCaptureDevice {
-    if UserDefaults.standard.bool(forKey: "use_telephoto"), let device = AVCaptureDevice.default(.builtInTelephotoCamera, for: .video, position: .back) {
-        return device
-    } else if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
-        return device
-    } else if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
-        return device
-    } else {
-        fatalError("Expected back camera device is not available.")
-    }
+  if UserDefaults.standard.bool(forKey: "use_telephoto"),
+    let device = AVCaptureDevice.default(.builtInTelephotoCamera, for: .video, position: .back)
+  {
+    return device
+  } else if let device = AVCaptureDevice.default(.builtInDualCamera, for: .video, position: .back) {
+    return device
+  } else if let device = AVCaptureDevice.default(
+    .builtInWideAngleCamera, for: .video, position: .back)
+  {
+    return device
+  } else {
+    fatalError("Expected back camera device is not available.")
+  }
 }
 
 public class VideoCapture: NSObject {
-    public var previewLayer: AVCaptureVideoPreviewLayer?
-    public weak var delegate: VideoCaptureDelegate?
+  public var previewLayer: AVCaptureVideoPreviewLayer?
+  public weak var delegate: VideoCaptureDelegate?
 
-    let captureDevice = bestCaptureDevice()
-    let captureSession = AVCaptureSession()
-    let videoOutput = AVCaptureVideoDataOutput()
-    var cameraOutput = AVCapturePhotoOutput()
-    let queue = DispatchQueue(label: "camera-queue")
-    
-    // Configures the camera and capture session with optional session presets.
-    public func setUp(sessionPreset: AVCaptureSession.Preset = .hd1280x720, completion: @escaping (Bool) -> Void) {
-        queue.async {
-            let success = self.setUpCamera(sessionPreset: sessionPreset)
-            DispatchQueue.main.async {
-                completion(success)
-            }
-        }
+  let captureDevice = bestCaptureDevice()
+  let captureSession = AVCaptureSession()
+  let videoOutput = AVCaptureVideoDataOutput()
+  var cameraOutput = AVCapturePhotoOutput()
+  let queue = DispatchQueue(label: "camera-queue")
+
+  // Configures the camera and capture session with optional session presets.
+  public func setUp(
+    sessionPreset: AVCaptureSession.Preset = .hd1280x720, completion: @escaping (Bool) -> Void
+  ) {
+    queue.async {
+      let success = self.setUpCamera(sessionPreset: sessionPreset)
+      DispatchQueue.main.async {
+        completion(success)
+      }
+    }
+  }
+
+  // Internal method to configure camera inputs, outputs, and session properties.
+  private func setUpCamera(sessionPreset: AVCaptureSession.Preset) -> Bool {
+    captureSession.beginConfiguration()
+    captureSession.sessionPreset = sessionPreset
+
+    guard let videoInput = try? AVCaptureDeviceInput(device: captureDevice) else {
+      return false
     }
 
-    // Internal method to configure camera inputs, outputs, and session properties.
-    private func setUpCamera(sessionPreset: AVCaptureSession.Preset) -> Bool {
-        captureSession.beginConfiguration()
-        captureSession.sessionPreset = sessionPreset
-
-        guard let videoInput = try? AVCaptureDeviceInput(device: captureDevice) else {
-            return false
-        }
-
-        if captureSession.canAddInput(videoInput) {
-            captureSession.addInput(videoInput)
-        }
-
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        previewLayer.videoGravity = .resizeAspectFill
-        previewLayer.connection?.videoOrientation = .portrait
-        self.previewLayer = previewLayer
-
-        let settings: [String: Any] = [
-            kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32BGRA)
-        ]
-
-        videoOutput.videoSettings = settings
-        videoOutput.alwaysDiscardsLateVideoFrames = true
-        videoOutput.setSampleBufferDelegate(self, queue: queue)
-        if captureSession.canAddOutput(videoOutput) {
-            captureSession.addOutput(videoOutput)
-        }
-
-        if captureSession.canAddOutput(cameraOutput) {
-            captureSession.addOutput(cameraOutput)
-        }
-        switch UIDevice.current.orientation {
-        case .portrait:
-            videoOutput.connection(with: .video)?.videoOrientation = .portrait
-        case .portraitUpsideDown:
-            videoOutput.connection(with: .video)?.videoOrientation = .portraitUpsideDown
-        case .landscapeRight:
-            videoOutput.connection(with: .video)?.videoOrientation = .landscapeLeft
-        case .landscapeLeft:
-            videoOutput.connection(with: .video)?.videoOrientation = .landscapeRight
-        default:
-            videoOutput.connection(with: .video)?.videoOrientation = .portrait
-        }
-        
-        if let connection = videoOutput.connection(with: .video) {
-            self.previewLayer?.connection?.videoOrientation = connection.videoOrientation
-        }
-        do {
-            try captureDevice.lockForConfiguration()
-            captureDevice.focusMode = .continuousAutoFocus
-            captureDevice.focusPointOfInterest = CGPoint(x: 0.5, y: 0.5)
-            captureDevice.exposureMode = .continuousAutoExposure
-            captureDevice.unlockForConfiguration()
-        } catch {
-            print("Unable to configure the capture device.")
-            return false
-        }
-
-        captureSession.commitConfiguration()
-        return true
+    if captureSession.canAddInput(videoInput) {
+      captureSession.addInput(videoInput)
     }
 
-    // Starts the video capture session.
-    public func start() {
-        if !captureSession.isRunning {
-            DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-                self?.captureSession.startRunning()
-            }
-        }
+    let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+    previewLayer.videoGravity = .resizeAspectFill
+    previewLayer.connection?.videoOrientation = .portrait
+    self.previewLayer = previewLayer
+
+    let settings: [String: Any] = [
+      kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32BGRA)
+    ]
+
+    videoOutput.videoSettings = settings
+    videoOutput.alwaysDiscardsLateVideoFrames = true
+    videoOutput.setSampleBufferDelegate(self, queue: queue)
+    if captureSession.canAddOutput(videoOutput) {
+      captureSession.addOutput(videoOutput)
     }
 
-    // Stops the video capture session.
-    public func stop() {
-        if captureSession.isRunning {
-            captureSession.stopRunning()
-        }
+    if captureSession.canAddOutput(cameraOutput) {
+      captureSession.addOutput(cameraOutput)
     }
-    
-    func updateVideoOrientation() {
-        guard let connection = videoOutput.connection(with: .video) else { return }
-        switch UIDevice.current.orientation {
-        case .portrait:
-            connection.videoOrientation = .portrait
-        case .portraitUpsideDown:
-            connection.videoOrientation = .portraitUpsideDown
-        case .landscapeRight:
-            connection.videoOrientation = .landscapeLeft
-        case .landscapeLeft:
-            connection.videoOrientation = .landscapeRight
-        default:
-            return
-        }
-        self.previewLayer?.connection?.videoOrientation = connection.videoOrientation
+    switch UIDevice.current.orientation {
+    case .portrait:
+      videoOutput.connection(with: .video)?.videoOrientation = .portrait
+    case .portraitUpsideDown:
+      videoOutput.connection(with: .video)?.videoOrientation = .portraitUpsideDown
+    case .landscapeRight:
+      videoOutput.connection(with: .video)?.videoOrientation = .landscapeLeft
+    case .landscapeLeft:
+      videoOutput.connection(with: .video)?.videoOrientation = .landscapeRight
+    default:
+      videoOutput.connection(with: .video)?.videoOrientation = .portrait
     }
+
+    if let connection = videoOutput.connection(with: .video) {
+      self.previewLayer?.connection?.videoOrientation = connection.videoOrientation
+    }
+    do {
+      try captureDevice.lockForConfiguration()
+      captureDevice.focusMode = .continuousAutoFocus
+      captureDevice.focusPointOfInterest = CGPoint(x: 0.5, y: 0.5)
+      captureDevice.exposureMode = .continuousAutoExposure
+      captureDevice.unlockForConfiguration()
+    } catch {
+      print("Unable to configure the capture device.")
+      return false
+    }
+
+    captureSession.commitConfiguration()
+    return true
+  }
+
+  // Starts the video capture session.
+  public func start() {
+    if !captureSession.isRunning {
+      DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+        self?.captureSession.startRunning()
+      }
+    }
+  }
+
+  // Stops the video capture session.
+  public func stop() {
+    if captureSession.isRunning {
+      captureSession.stopRunning()
+    }
+  }
+
+  func updateVideoOrientation() {
+    guard let connection = videoOutput.connection(with: .video) else { return }
+    switch UIDevice.current.orientation {
+    case .portrait:
+      connection.videoOrientation = .portrait
+    case .portraitUpsideDown:
+      connection.videoOrientation = .portraitUpsideDown
+    case .landscapeRight:
+      connection.videoOrientation = .landscapeLeft
+    case .landscapeLeft:
+      connection.videoOrientation = .landscapeRight
+    default:
+      return
+    }
+    self.previewLayer?.connection?.videoOrientation = connection.videoOrientation
+  }
 
 }
 
 // Extension to handle AVCaptureVideoDataOutputSampleBufferDelegate events.
 extension VideoCapture: AVCaptureVideoDataOutputSampleBufferDelegate {
-    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        delegate?.videoCapture(self, didCaptureVideoFrame: sampleBuffer)
-    }
+  public func captureOutput(
+    _ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer,
+    from connection: AVCaptureConnection
+  ) {
+    delegate?.videoCapture(self, didCaptureVideoFrame: sampleBuffer)
+  }
 
-    public func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // Optionally handle dropped frames, e.g., due to full buffer.
-    }
+  public func captureOutput(
+    _ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer,
+    from connection: AVCaptureConnection
+  ) {
+    // Optionally handle dropped frames, e.g., due to full buffer.
+  }
 }
